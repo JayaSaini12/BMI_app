3aaff229ca28cfde7ebd8b8ecfb4a872a3b88178
Jaya <jayarke890@gmail.com>
2023-06-30 13:05:16 +0530

    first commit

---
+++ lib/splash_screen.dart
@@ -0,0 +1,34 @@
+import 'dart:async';
+
+import 'package:bmi_app/main.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class SplashScreen extends StatefulWidget {
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    Timer(Duration(seconds: 5), () {
+      Navigator.pushReplacement(context, MaterialPageRoute(builder: (context)=> MyHomePage(title: "Your_BMI")));
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+          color: Colors.indigo,
+          child: Center(
+              child: Text(
+            "BodyCalc",
+            style: TextStyle(
+                fontSize: 34, fontWeight: FontWeight.w700, color: Colors.white),
+          ))),
+    );
+  }
+}
